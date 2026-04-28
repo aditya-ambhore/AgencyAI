65d28a826a9b53577be995c15de95a6c4c37cd51
aditya-ambhore <adityaambhore06@gmail.com>
2026-04-28 16:57:51 +0530

    Added Email Sent Popup

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,34 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import Title from "./Title";
 import assets from "../assets/assets";
 import toast from "react-hot-toast";
 import { motion } from "motion/react";
 
 const ContactUs = () => {
+  const [showPopup, setShowPopup] = useState(false);
+
   const onSubmit = async (event) => {
     event.preventDefault();
 
     const formData = new FormData(event.target);
 
     formData.append("access_key", "0aa08409-1252-4c71-b7c0-683c7c4c8cf3");
+    formData.append("subject", "New Contact Message");
+    formData.append("from_name", "Agency AI Website");
 
     try {
       const response = await fetch("https://api.web3forms.com/submit", {
         method: "POST",
+        headers: {
+          Accept: "application/json",
+        },
         body: formData,
       });
 
       const data = await response.json();
 
       if (data.success) {
-        toast.success("Thank you for your submission!");
+        toast.success("Message sent successfully ");
+        setShowPopup(true); // show popup
         event.target.reset();
       } else {
         console.log("Error", data);
         toast.error(data.message);
       }
     } catch (error) {
-      toast.error(error.message);
+      toast.error("Something went wrong ❌");
     }
   };
 
@@ -46,44 +54,45 @@ const ContactUs = () => {
         desc="From strategy to execution, we craft digital solutions that move your buisness forward."
       />
 
+      {/* FORM */}
       <motion.form
         initial={{ opacity: 0, y: 30 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5, delay: 0.4 }}
         viewport={{ once: true }}
         onSubmit={onSubmit}
-        className=" grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full"
+        className="grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full"
       >
         <div>
-          <p className="mb-2 text-sm font-medium ">Your name</p>
+          <p className="mb-2 text-sm font-medium">Your name</p>
           <div className="flex pl-3 rounded-lg border border-gray-300 dark:border-gray-600">
             <img src={assets.person_icon} alt="" />
             <input
               name="name"
               type="text"
               placeholder="Enter your name"
-              className="w-full p-3 text-sm outline-none "
+              className="w-full p-3 text-sm outline-none"
               required
             />
           </div>
         </div>
 
         <div>
-          <p className="mb-2 text-sm font-medium ">Email id</p>
+          <p className="mb-2 text-sm font-medium">Email id</p>
           <div className="flex pl-3 rounded-lg border border-gray-300 dark:border-gray-600">
             <img src={assets.email_icon} alt="" />
             <input
               name="email"
               type="email"
               placeholder="Enter your email"
-              className="w-full p-3 text-sm outline-none "
+              className="w-full p-3 text-sm outline-none"
               required
             />
           </div>
         </div>
 
         <div className="sm:col-span-2">
-          <p className="mb-2 text-sm font-medium ">Message</p>
+          <p className="mb-2 text-sm font-medium">Message</p>
           <textarea
             name="message"
             rows={8}
@@ -95,11 +104,32 @@ const ContactUs = () => {
 
         <button
           type="submit"
-          className="w-max flex gap-2 bg-primary text-white text-sm px-10 py-3 rounded-full cursor-pointer hover:scale-103 transition-all"
+          className="w-max flex gap-2 bg-primary text-white text-sm px-10 py-3 rounded-full cursor-pointer hover:scale-105 transition-all"
         >
           Submit <img src={assets.arrow_icon} alt="" className="w-4" />
         </button>
       </motion.form>
+
+      {/*  POPUP */}
+      {showPopup && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
+          <div className="bg-white dark:bg-gray-800 p-6 rounded-lg text-center shadow-lg max-w-sm w-full">
+            <h2 className="text-xl font-semibold mb-2 text-600">
+              Email Sent Successfully
+            </h2>
+            <p className="text-sm text-gray-600 dark:text-gray-300 mb-4">
+              Thank you! Your message has been sent.
+            </p>
+
+            <button
+              onClick={() => setShowPopup(false)}
+              className="bg-primary text-white px-4 py-2 rounded"
+            >
+              OK
+            </button>
+          </div>
+        </div>
+      )}
     </motion.div>
   );
 };
